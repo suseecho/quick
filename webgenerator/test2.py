d1f9c7725db8e73bed360c4734b5b35455e1b100
echo <suseecho@sina.com>
2018-02-08 21:39:21 +0800

    生成网页“ “

--- webgenerator/test2.py
+++ webgenerator/test2.py
@@ -7,13 +7,13 @@ handle=os.walk(".")
 for root,dir,files in handle:
     for file in files:
         #temp=file
-        if(file.split(".")[-1] == "mkv" or file.split(".",1)[1] == "avi" or file.split(".",1)[-1] == "mp4" or file.split(".",1)[1] == "MP4"):
+        if(file.split(".")[-1] == "mkv" or file.split(".")[-1] == "avi" or file.split(".")[-1] == "mp4" or file.split(".")[-1] == "MP4"):
             print("<p>视频</p>")
             print('''<video width="960" height="720" controls="controls" 
 src="''',end="")
             print(file,end="")
             print('''"></video></object>''')
-        if(file.split(".")[-1] == "jpg" or file.split(".",1)[1] == "JPG" or file.split(".",1)[-1] == "PNG" or file.split(".",1)[1] == "png"):
+        if(file.split(".")[-1] == "jpg" or file.split(".")[-1] == "JPG" or file.split(".")[-1] == "PNG" or file.split(".")[-1] == "png"):
             print('''<img src="''',end="")
             print(file,end="")
             print('''"</img>''')
